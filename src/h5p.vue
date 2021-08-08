5f50c9414e8b1bec4d78c49ad81109bc8a724a2d
JaumeRF <jaume.rf16@gmail.com>
2021-08-08 20:03:40 +0200

    Cambiada la tipografía

--- src/h5p.vue
+++ src/h5p.vue
@@ -130,7 +130,7 @@ export default {
     const contentStyles = styles.map(style => `<link rel="stylesheet" href="${style}">`).join('\n')
     const contentScripts = scripts.map(script => `<script src="${script}">${endScript}`).join('\n')
     this.srcdoc = `<!doctype html>
-<html class="h5p-iframe">
+<html class="h5p-iframe" >
   <head>
     <base target="_parent">
     <style>${frameStyle}</style>
@@ -140,7 +140,7 @@ export default {
     ${contentScripts}
   </head>
   <body>
-    <div class="h5p-content" data-content-id="default"/>
+    <div class="h5p-content" style="font-family: 'Montserrat', sans-serif !important;" data-content-id="default"/>
   </body>
 </html>`
 
