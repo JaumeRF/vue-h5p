7c627b2e15b4ce35c212a267c4d2fec3b61f5100
JaumeRF <jaume.rf16@gmail.com>
2021-10-13 11:24:39 +0200

    Ahora sí

--- src/h5p.vue
+++ src/h5p.vue
@@ -132,6 +132,9 @@ export default {
     this.srcdoc = `<!doctype html>
 <html class="h5p-iframe" >
   <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <base target="_parent">
     <style>${frameStyle}</style>
     ${contentStyles}
